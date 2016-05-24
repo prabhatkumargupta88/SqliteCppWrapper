6b2722a9396ec7c18c24b8d2a9df2cdb24ee14db
Prabhat Kumar Gupta <prabhatkumargupta@prabhats-macbook-air.local>
2016-05-24 23:58:32 +0530

    Initial Commit

---
+++ SqliteCppWrapper/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SqliteCppWrapper
+//
+//  Created by Prabhat Kumar Gupta on 24/05/16.
+//  Copyright © 2016 Prabhat Kumar Gupta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
